a9732fc5c2172ba4fec649b7a6fe93ed8c492bab
johnmaxnew <forevermeticulous@gmail.com>
2021-10-16 13:53:34 +0300

    modified components

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,13 @@ import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 function Navbar() {
+    const [click, setClick] = useState(false);
+
+    const HandleClick = () => setClick(!click);
+
+    const closeMobileMenu = () => setClick(false);
+    
+    
     return (
         <div>
             <div className="nav">
@@ -9,6 +16,31 @@ function Navbar() {
                     <Link to="/" className="nav__logo">
                         Lagoon
                     </Link>
+                    <div className="menu__icon" onClick={HandleClick}>
+                        <i className={ click ? "fas fa-times": "fas fa-bars"}></i>
+                    </div>
+                    <ul className={ click ? "menu active": "menu"}>
+                        <li className='nav-item'>
+                            <Link to="/" className="nav-link" onClick={closeMobileMenu}>
+                                Home
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to="/" className="nav-link" onClick={closeMobileMenu}>
+                                About
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to="/" className="nav-link" onClick={closeMobileMenu}>
+                                Projects
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to="/" className="nav-link" onClick={closeMobileMenu}>
+                                Prices
+                            </Link>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </div>
