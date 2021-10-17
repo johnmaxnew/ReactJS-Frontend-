b1323a44dac24a7c3a2108e790ebc8eb4e5fe12f
johnmaxnew <forevermeticulous@gmail.com>
2021-10-17 23:30:23 +0300

    modified components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import './App.css';
-// import Home from './components/pages/Home';
+import Home from './components/pages/Home';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-// import Services from './components/pages/Services';
-// import Products from './components/pages/Products';
-// import SignUp from './components/pages/SignUp';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
 
 import Navbar from './components/Navbar';
-import Home from './components/pages/Home';
 
 function App() {
   return (
@@ -16,9 +15,9 @@ function App() {
         <Navbar/>
         <Switch>
           <Route path='/' exact component={Home} />
-          {/* <Route path='/services' component={Services} />
+          <Route path='/services' component={Services} />
           <Route path='/products' component={Products} />
-          <Route path='/sign-up' component={SignUp} /> */}
+          <Route path='/sign-up' component={SignUp} />
         </Switch>
       </Router>
     </>
